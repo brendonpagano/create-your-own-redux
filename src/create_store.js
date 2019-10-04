a0dcb8c4c3ef8537da91b3ecf7cce0f65f591b2e
annatsu <brendonpagano@gmail.com>
2019-10-04 19:06:08 -0300

    Make a bad looking createStore function

--- src/create_store.js
+++ src/create_store.js
@@ -1,9 +1,36 @@
 /**
+ * Creates a Redux store.
  *
  * @param {Function} reducer
  * @param {any} preloadedState
  * @param {Function} enhancer
+ *
+ * @return {Object}
  */
-function createStore(reducer, preloadedState, enhancer) {}
+function createStore(reducer, preloadedState, enhancer) {
+  let state = preloadedState || {};
+
+  const storeListeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    const newState = reducer(action, state);
+    state = newState;
+    storeListeners.forEach((fn) => fn());
+  };
+
+  const subscribe = (listener) => {
+    const currentListenersIndex = storeListeners.length;
+    storeListeners.push(listener);
+    return () => storeListeners.splice(currentListenersIndex, 1);
+  };
+
+  return Object.freeze({
+    getState,
+    dispatch,
+    subscribe,
+  });
+}
 
 module.exports = createStore;
