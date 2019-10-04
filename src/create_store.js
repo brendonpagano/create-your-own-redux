e99fb85bb541c64ce15614073a4b19dcec8ce43c
annatsu <brendonpagano@gmail.com>
2019-10-04 18:55:36 -0300

    Create base structure for testing

---
+++ src/create_store.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * @param {Function} reducer
+ * @param {any} preloadedState
+ * @param {Function} enhancer
+ */
+function createStore(reducer, preloadedState, enhancer) {}
+
+module.exports = createStore;
